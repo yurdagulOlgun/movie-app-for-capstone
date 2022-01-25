4d9cb6808591fb294a1e9b4ea1d58721d5214fdb
Yurdagül OLGUN <yurdagulolgun98@gmail.com>
2022-01-25 23:09:30 +0300

    filter by and sort by on the popular page

--- src/components/pages/Popular.jsx
+++ src/components/pages/Popular.jsx
@@ -1,6 +1,16 @@
 import { useQuery } from "react-query";
 import { fetchPopularMovies } from "../../data";
-import { Col, Card, Container, Row, Dropdown  } from "react-bootstrap";
+import {
+  Col,
+  Card,
+  Container,
+  Row,
+  Dropdown,
+  DropdownButton,
+  Button,
+  ButtonGroup,
+} from "react-bootstrap";
+import { useState } from "react";
 
 export default function TopRated() {
   const { data } = useQuery("movies", fetchPopularMovies, {
@@ -8,30 +18,71 @@ export default function TopRated() {
     select: (data) => data.data.results,
   });
 
+  const [value, setValue] = useState("");
+  const handleSelect = (e) => {
+    console.log(e);
+    setValue(e);
+  };
+
   return (
     <>
       <Container className="mt-3">
-        <Row >
+        <Row>
           <h1 className="text-center mb-3">Popular Movies</h1>
           <Col className=" border border-warning ">
-            <Dropdown className="p-2">
-              <Dropdown.Toggle variant="warning" id="dropdown-basic">
-                Sort By
-              </Dropdown.Toggle>
-              <Dropdown.Menu >
-                <Dropdown.Item href="#/action-1" >Filmin başlığı A'dan Z'ye, </Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Filmin başlığı Z'den A'ya, </Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Popülerliği göre artan, </Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Popülerliği göre azalan, </Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Çıkış tarihine göre artan, </Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Çıkış tarihine göre azalan</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-            <Row>
-              <Col> 
+            <DropdownButton
+              variant="warning"
+              alignRight
+              title={`${value}` ? `${value}` : `sort by`}
+              id="dropdown-menu-align-right"
+              onSelect={handleSelect}
+              className="mt-4"
+            >
+              <Dropdown.Item eventKey="A to Z by The Title ">
+                A to Z by The Title
+              </Dropdown.Item>
+              <Dropdown.Item eventKey="Z to A by The Title ">
+                Z to A by The Title
+              </Dropdown.Item>
+              <Dropdown.Item eventKey="Increasing by Popularity">
+                Increasing by Popularity
+              </Dropdown.Item>
+              <Dropdown.Item eventKey="Decreasing by Popularity">
+                Decreasing by Popularity
+              </Dropdown.Item>
+              <Dropdown.Item eventKey="Increasing by Release Date">
+                Increasing by Release Date
+              </Dropdown.Item>
+              <Dropdown.Item eventKey="Decreasing by Release Date">
+                Decreasing by Release Date
+              </Dropdown.Item>
+            </DropdownButton>
+            <Row className="mt-3">
+              <div className=" d-flex flex-column">
                 <h3>Filter By</h3>
-              </Col>
+               <label htmlFor="text" className="mt-3">From:</label>
+               <label htmlFor="text" className="mt-3">To:</label>
+              </div>
             </Row>
+            <ButtonGroup size="sm" >
+              <Button className="m-2" variant="warning">Action</Button>
+              <Button className="m-2" variant="warning">Adventure</Button>
+              <Button className="m-2" variant="warning">Comedy</Button>
+            </ButtonGroup>
+            <ButtonGroup size="sm">
+              <Button className="m-2" variant="warning">Romance</Button>
+              <Button className="m-2" variant="warning">Drama</Button>
+              <Button className="m-2" variant="warning">Comedy</Button>
+            </ButtonGroup>
+            <ButtonGroup size="sm">
+              <Button className="m-2" variant="warning">Horror</Button>
+              <Button className="m-2" variant="warning">Science Fiction</Button>
+              <Button className="m-2" variant="warning">War</Button>
+            </ButtonGroup>
+            <Col>
+              <Button className="mt-5" variant="secondary" size="sm">Search</Button>
+            </Col>
+              
             
           </Col>
           {data?.map((item) => (
